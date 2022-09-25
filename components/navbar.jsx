7f7f914d4683d3fd3586b1549efef10123962c51
Gonçalo Albuquerque <gjca01@hotmail.com>
2022-09-25 12:09:26 +0100

    Some animations, fixed navbar and about section

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -13,9 +13,9 @@ const navbar = () => {
     }
 
     return (
-        <div  className='fixed w-full h-20 shadow-xl z-[100] bg-[#4d4740]'>
-            <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-                <Image src="/../public/assets/logo.png" alt="/" width='125' height='50' /> 
+        <div className='fixed w-full h-auto shadow-xl z-[100] bg-[#4d4740]'>
+            <div className='flex justify-between items-center w-full h-full  2xl:px-16  p-2 hover:scale-105 ease-in duration-300'>
+                <Image src="/../public/assets/GA_2.png" alt="/" width='110' height='110' /> 
                 <div>
                     <ul className='hidden md:flex mr-20'>
                         <Link href='/'>
@@ -35,23 +35,23 @@ const navbar = () => {
                         </Link>
                     </ul>
                     <div onClick={handleNav} className='md:hidden mr-10'>
-                        <AiOutlineMenu size={25}></AiOutlineMenu>
+                        <AiOutlineMenu color='#f7e5ce' size={25}></AiOutlineMenu>
                     </div>
                 </div>
             </div>
-            <div className={nav ? ' md:hidden fixed left-0 top-0 w-full h-screen bg-black/50': ''}>
+            <div className={nav ? ' md:hidden fixed left-0 top-0 w-full h-screen bg-[#4d4740]/80': ''}>
                 <div className={nav ? 
-                    'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#f7e5ce] p-10 ease-in duration-300' :
-                    'fixed left-[-100%] top-0 p-10 ease-in duration-300'}>
+                    'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#f7e5ce] p-10 ease-in duration-500' :
+                    'fixed left-[-100%] top-0 p-10 ease-out duration-500'}>
                     <div>
                         <div className='flex w-full items-center justify-between'>
-                            <Image src='/../public/assets/logo.png' width='150' height='150' alt='/'/>
+                            <Image src='/../public/assets/GA_1.png' width='150' height='150' alt='/'/>
                             <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer '>
-                                <AiOutlineClose size={25}></AiOutlineClose>
+                                <AiOutlineClose color='#4d4740' size={25}></AiOutlineClose>
                             </div>
                         </div>
                         <div className='border-b border-gray-400 '>
-                            <h3 className='w-[85%] md:w-[90%] py-4 text-[#191715]'>Let's build something together</h3>
+                            <h3 className='w-[85%] md:w-[90%] py-4 text-[#4d4740]'>Let's build something together</h3>
                         </div>
                         <div className='py-4 flex flex-col'>
                             <ul className='uppercase'>
@@ -72,7 +72,7 @@ const navbar = () => {
                                 </Link>
                             </ul>
                             <div className='pt-40'>
-                                <h3 className='uppercase  text-[#191715]'>Feel free to connect</h3>
+                                <h3 className='uppercase  text-[#4d4740]'>Feel free to connect</h3>
                                 <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
                                     <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
                                         <FaLinkedinIn></FaLinkedinIn>
